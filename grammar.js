62f49eace1b9b01c2bacf7d599120be05df40beb
Michael Davis <mcarsondavis@gmail.com>
2021-12-23 19:35:39 -0600

    reorganize 'seq's with 'iseq' helper

--- grammar.js
+++ grammar.js
@@ -2,17 +2,6 @@ const NEWLINE = /\r?\n/;
 const WHITE_SPACE = /[\t\f\v ]+/;
 const ANYTHING = /[^\r\n]+/;
 
-const COMMAND_PRELUDE = token.immediate("diff");
-const FILE_CHANGE_PRELUDE = token.immediate(
-  field("kind", choice("new", "deleted", "rename"))
-);
-const INDEX_PRELUDE = token.immediate("index");
-const SIMILARITY_PRELUDE = token.immediate("similarity");
-const FILE_PRELUDE = token.immediate(field("kind", choice("---", "+++")));
-const LOCATION_PRELUDE = token.immediate("@@");
-const ADDITION_PRELUDE = token.immediate("+");
-const DELETION_PRELUDE = token.immediate("-");
-
 module.exports = grammar({
   name: "gitdiff",
 
@@ -34,34 +23,39 @@ module.exports = grammar({
         $.context
       ),
 
-    command: ($) => seq(COMMAND_PRELUDE, "--git", $.filename),
+    command: ($) => iseq("diff", "--git", $.filename),
 
     file_change: ($) =>
-      choice(
-        seq(FILE_CHANGE_PRELUDE, "file", "mode", $.mode),
-        seq(FILE_CHANGE_PRELUDE, choice("from", "to"), $.filename)
+      iseq(
+        field("kind", choice("new", "deleted", "rename")),
+        choice(
+          seq("file", "mode", $.mode),
+          seq(choice("from", "to"), $.filename)
+        )
       ),
 
-    index: ($) =>
-      seq(INDEX_PRELUDE, $.commit, "..", $.commit, optional($.mode)),
 
-    similarity: ($) =>
-      seq(SIMILARITY_PRELUDE, "index", field("score", /\d+/), "%"),
+    index: ($) => iseq("index", $.commit, "..", $.commit, optional($.mode)),
 
-    file: ($) => seq(FILE_PRELUDE, $.filename),
+    similarity: ($) => iseq("similarity", "index", field("score", /\d+/), "%"),
 
-    location: ($) =>
-      seq(LOCATION_PRELUDE, $.linerange, $.linerange, "@@", ANYTHING),
+    file: ($) => iseq(field("kind", choice("---", "+++")), $.filename),
 
-    addition: ($) => seq(ADDITION_PRELUDE, optional(ANYTHING)),
+    location: ($) => iseq("@@", $.linerange, $.linerange, "@@", ANYTHING),
 
-    deletion: ($) => seq(DELETION_PRELUDE, optional(ANYTHING)),
+    addition: ($) => iseq("+", optional(ANYTHING)),
+
+    deletion: ($) => iseq("-", optional(ANYTHING)),
 
     context: ($) => token(prec(-1, ANYTHING)),
 
     linerange: ($) => /[-\+]\d+,\d+/,
-    filename: ($) => ANYTHING,
+    filename: ($) => repeat1(/\S+/),
     commit: ($) => /[a-f0-9]{7,40}/,
     mode: ($) => /\d+/,
   },
 });
+
+function iseq(start_token, ...tokens) {
+  return seq(token.immediate(start_token), ...tokens);
+}
